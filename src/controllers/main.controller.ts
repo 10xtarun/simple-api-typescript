2c09dcbe6658532e5a22c9e3fa56baf8b032197f
10xtarun <10xtarun@gmail.com>
2020-06-13 11:36:12 +0530

    DELETE Route added

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -10,7 +10,11 @@ export class Controller {
     }
 
     public routes() {
+        //@GET /bookmarks   get all bookmarks
         this.app.route('/bookmarks').get(this.apiService.getAllBookmarks);
+        //@POST /bookmarks  save a bookmark
         this.app.route('/bookmarks').post(this.apiService.saveBookmark);
+        //@DELETE /bookmarks/delete/:id     delete a bookmark 
+        this.app.route('/bookmarks/delete/:id').delete(this.apiService.deleteBookmark);
     }
 }
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,8 +3,8 @@ import { MongooseDocument } from "mongoose";
 //import model
 import { Bookmark } from "../models/bookmark.model";
 export class APIService {
+    //@method get all bookmarks
     public getAllBookmarks(req: Request, res: Response){
-        console.log("inside get all");
         Bookmark.find({}, (error: Error, data: MongooseDocument) => {
             if(error){
                 return res.json({
@@ -20,7 +20,7 @@ export class APIService {
                 })
         });
     }
-
+    //@method save a bookmark
     public saveBookmark(req: Request, res: Response) {
         let name = req.body.name;
         let url = req.body.url;
@@ -44,6 +44,23 @@ export class APIService {
                 data: newBookmark,
             })
         })
-
     }
+    //@method delete particular bookmarks
+    public deleteBookmark(req: Request, res: Response){
+        let id = req.params.id;
+        Bookmark.deleteOne({"_id": id}, (err) => {
+            if(err){
+                return res.json({
+                    status: 500,
+                    success: false,
+                    message: "Unable to delete document",
+                })
+            }
+            res.json({
+                status: 200,
+                success: true,
+                message: "Document deleted successfully",
+            })
+        } );
+    } 
 }
